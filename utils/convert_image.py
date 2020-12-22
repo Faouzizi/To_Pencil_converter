e597ab2d0cb97ef60b1362f5b17e53cbf322adc8
faouzi <elmbarkif@gmail.com>
2020-12-22 02:34:52 +0100

    correct convert_image file

--- utils/convert_image.py
+++ utils/convert_image.py
@@ -3,27 +3,28 @@
 ########################################################################################
 import cv2
 
-def convert_images(path_import, image_name):
-    ##############################################################################
-    ############# Import images
-    ##############################################################################
-    color_image = cv2.imread(path_import)
+class convert_images():
+    def get_image_converted(self,path_import, image_name):
+        ##############################################################################
+        ############# Import images
+        ##############################################################################
+        color_image = cv2.imread(path_import)
 
-    ##############################################################################
-    ############# cartoonify images
-    ##############################################################################
-    cartoon_image = cv2.stylization(color_image, sigma_s=150, sigma_r=0.01)
+        ##############################################################################
+        ############# cartoonify images
+        ##############################################################################
+        cartoon_image = cv2.stylization(color_image, sigma_s=150, sigma_r=0.01)
 
-    ##############################################################################
-    ############# pencilfy images
-    ##############################################################################
-    cartoon_image1, cartoon_image2  = cv2.pencilSketch(color_image, sigma_s=60, sigma_r=0.5, shade_factor=0.01)
+        ##############################################################################
+        ############# pencilfy images
+        ##############################################################################
+        cartoon_image1, cartoon_image2  = cv2.pencilSketch(color_image, sigma_s=60, sigma_r=0.5, shade_factor=0.01)
 
-    ##############################################################################
-    ############# export results images
-    ##############################################################################
-    cv2.imwrite('./results/'+image_name+'_pencil1'+'.jpg', cartoon_image1)
-    cv2.imwrite('./results/'+image_name+'_pencil2'+'.jpg', cartoon_image2)
-    cv2.imwrite('./results/'+image_name+'_cartoon'+'.jpg', cartoon_image)
+        ##############################################################################
+        ############# export results images
+        ##############################################################################
+        cv2.imwrite('./results/'+image_name+'_pencil1'+'.jpg', cartoon_image1)
+        cv2.imwrite('./results/'+image_name+'_pencil2'+'.jpg', cartoon_image2)
+        cv2.imwrite('./results/'+image_name+'_cartoon'+'.jpg', cartoon_image)
 
 
